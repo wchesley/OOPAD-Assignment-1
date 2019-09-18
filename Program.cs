4694dee867dba7ca993f8b4d976d5e3917c250e6
wchesley <wrchesley1@buffs.wtamu.edu>
2019-09-18 09:52:04 -0500

    main method complete, time to debug

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Assignment_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tDate d1 = new tDate(12,15,1990);
+            d1.showDate("E");
+            d1.showDate("N");
+            tDate d2 = new tDate("12/15/1990");
+            d2.showDate(1);
+            d2.showDate(0);
+            tDate d3 = new tDate("15/12/1990");
+            d3.showDate(1);
+            d3.showDate(0);
+            tDate d4 = new tDate("11/11/1811");
+            d4.showDate(1);
+            d4.showDate(0);
         }
     }
 }
